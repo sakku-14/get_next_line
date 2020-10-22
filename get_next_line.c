cc745eb477e2cf7038dd4618a3599fa55ff53171
Yuki Sakuma <kumasan.04.24.14@gmail.com>
2020-10-22 18:55:35 +0900

    [Add] error requirement

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: ysakuma <ysakuma@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/22 13:39:50 by ysakuma           #+#    #+#             */
-/*   Updated: 2020/10/22 13:40:37 by ysakuma          ###   ########.fr       */
+/*   Updated: 2020/10/22 18:53:41 by ysakuma          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,21 @@
 
 int get_next_line(int fd, char **line)
 {
-	
+	ssize_t len;
+	static char *buf;
+
+	if (!line || fd < 0 || fd > FD_MAX)
+		//error action
+	if (!(buf = malloc(sizeof(char) * (BUFFER_SIZE + 1))))
+		//error action
+	len = 0;
+	if (!(len = read(fd, buf, BUFFER_SIZE)) < 0)
+		//error action
+	while (*buf)
+	{
+		if (*buf == '\n')
+			break;
+		*line++ = *buf++;
+	}
+
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,8 +6,23 @@
 /*   By: ysakuma <ysakuma@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/22 13:43:50 by ysakuma           #+#    #+#             */
-/*   Updated: 2020/10/22 13:43:52 by ysakuma          ###   ########.fr       */
+/*   Updated: 2020/10/22 18:53:50 by ysakuma          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef _GET_NEXT_LINE_H
+# define _GET_NEXT_LINE_H
+# include <unistd.h>
+# include <stdlib.h>
+# include <limits.h>
 
+# ifndef MAX_FD
+#  define MAX_FD 1024
+# endif
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+
+int		get_next_line(int fd, char **line);
+
+#endif
