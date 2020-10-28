f67044bfc8b3c90167a99463c4595d051f4209a3
Yuki Sakuma <kumasan.04.24.14@gmail.com>
2020-10-28 19:30:42 +0900

    [Add] gnl_overwite

--- get_next_line.c
+++ get_next_line.c
@@ -6,11 +6,32 @@
 /*   By: ysakuma <ysakuma@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/22 13:39:50 by ysakuma           #+#    #+#             */
-/*   Updated: 2020/10/28 17:06:42 by ysakuma          ###   ########.fr       */
+/*   Updated: 2020/10/28 19:30:32 by ysakuma          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+
+int gnl_overwite(char *save, char **line, char *buf)
+{
+	size_t	len;
+	size_t	len_after;
+	char	*tmp;
+
+	len = 0;
+	while (*save != '\n')
+		len++
+	if (!(*line = malloc(sizeof(char) * (len + 1))))
+		//error処理
+	ft_strlcpy(*line, save, len + 1);
+	len_after = ft_strlen(save + len + 1);
+	if (!(tmp = malloc(sizeof(char) * (len_after + 1))))
+		//error処理
+	ft_strlcpy(tmp, save + len + 1, len_after + 1);
+	save = tmp;
+	free(buf);
+	return (1);
+}
 
 int get_next_line(int fd, char **line)
 {
@@ -19,13 +40,20 @@ int get_next_line(int fd, char **line)
 	char *buf;
 
 	if (!line || fd < 0 || fd > FD_MAX)
-		return (-1);)
+		return (-1);
 	if (!save[fd])
-		//
+		if (!(s[fd] = ft_calloc(1, sizeof(char))))
+			return (-1);
 	if (!(buf = malloc(BUFFER_SIZE)))
-		//error action
+	{
+		free(save[fd]);
+		save[fd] = NULL;
+		return (-1);
+	}
 	if (ft_memchr(save[fd], '\n', ft_strlen(save[fd])))
-		//'\n'があった時の処理
+	{
+		return (gnl_overwite(save[fd], line, buf));
+	}
 	while ((len = read(fd, buf, BUFFER_SIZE)) >= 0)
 	{
 		//buf内をs[fd]へくっつける
