0600ec8538c4253a2bc416ed07b82db6060a4f5b
Yuki Sakuma <kumasan.04.24.14@gmail.com>
2020-10-29 14:41:40 +0900

    [Add] utils

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: ysakuma <ysakuma@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/22 13:43:50 by ysakuma           #+#    #+#             */
-/*   Updated: 2020/10/23 14:02:41 by ysakuma          ###   ########.fr       */
+/*   Updated: 2020/10/29 14:41:22 by ysakuma          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,5 +24,10 @@
 # endif
 
 int		get_next_line(int fd, char **line);
+void	*ft_calloc(size_t count, size_t size);
+void	*ft_memset(void *b, int c, size_t len);
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
+size_t	ft_strlen(const char *s);
+void	*ft_memchr(const void *s, int c, size_t n);
 
 #endif
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,8 +6,72 @@
 /*   By: ysakuma <ysakuma@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/22 13:43:34 by ysakuma           #+#    #+#             */
-/*   Updated: 2020/10/22 13:43:36 by ysakuma          ###   ########.fr       */
+/*   Updated: 2020/10/29 14:39:17 by ysakuma          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+void	*ft_calloc(size_t count, size_t size)
+{
+	void *ret;
 
+	if (!count || !size)
+	{
+		count = 1;
+		size = 1;
+	}
+	if (!(ret = malloc(count * size)))
+		return (NULL);
+	ft_memset(ret, 0, count * size);
+	return (ret);
+}
+
+void	*ft_memset(void *b, int c, size_t len)
+{
+	unsigned char *bb;
+
+	bb = (unsigned char*)b;
+	while (len--)
+		*bb++ = (unsigned char)c;
+	return (b);
+}
+
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
+{
+	char	*src_c;
+	int		len;
+
+	src_c = (char *)src;
+	len = ft_strlen(src);
+	while (dstsize > 1 && *src_c)
+	{
+		*dst++ = *src_c++;
+		dstsize--;
+	}
+	if (dstsize)
+		*dst = '\0';
+	return (len);
+}
+
+size_t	ft_strlen(const char *s)
+{
+	size_t len;
+
+	len = 0;
+	while (*s++)
+		len++;
+	return (len);
+}
+
+void	*ft_memchr(const void *s, int c, size_t n)
+{
+	unsigned char	*s_c;
+
+	s_c = (unsigned char *)s;
+	while (n--)
+	{
+		if (*s_c++ == (unsigned char)c)
+			return ((void*)s);
+		s++;
+	}
+	return (NULL);
+}
