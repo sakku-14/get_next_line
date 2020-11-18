c728047806c02c219956ed70684974696636385c
Yuki Sakuma <kumasan.04.24.14@gmail.com>
2020-11-18 10:22:40 +0900

    [Add] for test on gwacamore

---
+++ main.c
@@ -0,0 +1,80 @@
+#include "get_next_line.h"
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <string.h>
+
+int main(int ac, char **av)
+{
+	int fd = open(av[1], O_RDONLY);
+	char *line;
+	while (get_next_line(fd, &line))
+	{
+		printf("%s\n", line);
+		free(line);
+	}
+	printf("%s\n", line);
+	free(line);
+	close(fd);
+	ac = 0;
+	return (0);
+}
+
+//some av ver totyuuuuuuuuuuuuuuuuuuuuuu
+/*
+int main(int ac, char **av)
+{
+	int		fd[MAX_FD];
+	char	*line;
+	int		i, flag = 0;
+	int		ret;
+
+	while(i + 1 < ac)
+	{
+		fd[i] = open(av[i + 1], O_RDONLY);
+		i++;
+	}
+	i = 0;
+	while(1)
+	{
+		while(i + 1 < ac)
+		{
+			if((ret = get_next_line(fd[i], &line)) == -1)
+				flag = 1;
+			if (flag == 0)
+			{
+				printf("fd%d:%d:%s\n", i, ret, line);
+				free(line);
+				i++;
+			}
+			else
+			{
+				printf("fd%d:%d:...error\n", i, ret);
+			}
+			flag = 0;
+		}
+		i = 0;
+	}
+	i = 0;
+	while (i + 1 < ac)
+	{
+		close(fd[i]);
+		i++;
+	}
+		return (0);
+}
+*/
+
+//stdin ver
+/*
+int main(int ac, char **av)
+{
+	int fd = 0;
+	char *line;
+	while (get_next_line(fd, &line))
+		printf("%s\n", line);
+	printf("%s\n", line);
+	return (0);
+}
+*/
---
+++ test.c
@@ -0,0 +1,76 @@
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include "get_next_line.h"
+
+#ifndef N_FDMAX
+# define N_FDMAX 12800
+#endif
+
+int		main(int argc, char **argv)
+{
+	int		i;
+	int		j;
+	int		fd[N_FDMAX];
+	int		flag_notfinished;
+	char	*line;
+	int		res;
+	int		flag_stdin;
+
+	if (argc == 1)
+	{
+		fd[0] = 0;
+		argc = 2;
+		flag_stdin = 1;
+	}
+	else
+	{
+		i = 0;
+		while (i < argc - 1)
+		{
+			fd[i] = open(argv[i + 1], O_RDONLY);
+			i++;
+		}
+	}
+	flag_notfinished = 1;
+	i = 0;
+	while (flag_notfinished)
+	{
+		flag_notfinished = 0;
+		printf("GNL: %d\n", i);
+		j = 0;
+		while (j < argc - 1)
+		{
+			if (fd[j] != -2)
+			{
+				res = get_next_line(fd[j], &line);
+				printf("fd-%03d: res = %2d", fd[j], res);
+				if (res == -1)
+				{
+					printf(", ERROR!!\n");
+					fd[j] = -2;
+				}
+				else
+				{
+					flag_notfinished += res;
+					printf(", line = \"%s\"\n", line);
+					free(line);
+					if (res == 0)
+					{
+						if (fd[j] > 2)
+							close(fd[j]);
+						fd[j] = -2;
+					}
+				}
+			}
+			j++;
+		}
+		printf("\n");
+		i++;
+	}
+	printf("finished\n");
+	#ifdef LEAKCHECK
+	while(1);
+	#endif
+}
